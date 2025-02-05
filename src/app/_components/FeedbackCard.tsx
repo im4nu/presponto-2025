f846078d3de9aa1bfc25328e1ad7d7c23297e58f
manu <victorx811@gmail.com>
2025-02-05 15:08:27 -0300

    build error solved again

---
+++ src/app/_components/FeedbackCard.tsx
@@ -0,0 +1,41 @@
+import { StarIcon } from "@heroicons/react/16/solid";
+import { cn } from "~/lib/utils";
+
+interface PostProps {
+  inverted?: boolean;
+}
+
+export default function FeedbackCard({ inverted }: PostProps) {
+  return (
+    <div
+      className={cn(
+        "flex h-[350px] w-full items-start justify-between gap-4",
+        inverted && "flex-row-reverse",
+      )}
+    >
+      <div className="flex h-full w-[60%] rounded-lg border border-white bg-white/40"></div>
+
+      <div className="flex w-[40%] flex-col items-center justify-start gap-6 py-4">
+        <div className="flex w-full flex-col">
+          <p className="text-2xl font-bold">Emmanuel Rodrigues</p>
+          <p className="text-xs text-white/70">01/02/2025</p>
+        </div>
+
+        <div className="flex w-full flex-col">
+          <p className="text-lg">Avaliação</p>
+          <p>5 Estelas</p>
+          <div className="flex">
+            {new Array(5).fill(0).map((_, i) => (
+              <StarIcon className="h-4 w-4 fill-white/80" key={i} />
+            ))}
+          </div>
+        </div>
+
+        <div className="flex w-full flex-col">
+          <p className="text-2xl font-bold">Já é cliente a:</p>
+          <p>Mais de 5 anos</p>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,11 @@
 
 // import { LatestPost } from "~/app/_components/post";
 // import { api, HydrateClient } from "~/trpc/server";
-import { ChevronDoubleDownIcon, StarIcon } from "@heroicons/react/16/solid";
+import { ChevronDoubleDownIcon } from "@heroicons/react/16/solid";
 import Image from "next/image";
 import { Button } from "~/components/ui/button";
-import { cn } from "~/lib/utils";
 import { HydrateClient } from "~/trpc/server";
+import FeedbackCard from "./_components/FeedbackCard";
 
 export default async function Home() {
   // const hello = await api.post.hello({ text: "from tRPC" });
@@ -73,44 +73,5 @@ export default async function Home() {
         </section>
       </main>
     </HydrateClient>
-  );
-}
-
-interface PostProps {
-  inverted?: boolean;
-}
-
-function FeedbackCard({ inverted }: PostProps) {
-  return (
-    <div
-      className={cn(
-        "flex h-[350px] w-full items-start justify-between gap-4",
-        inverted && "flex-row-reverse",
-      )}
-    >
-      <div className="flex h-full w-[60%] rounded-lg border border-white bg-white/40"></div>
-
-      <div className="flex w-[40%] flex-col items-center justify-start gap-6 py-4">
-        <div className="flex w-full flex-col">
-          <p className="text-2xl font-bold">Emmanuel Rodrigues</p>
-          <p className="text-xs text-white/70">01/02/2025</p>
-        </div>
-
-        <div className="flex w-full flex-col">
-          <p className="text-lg">Avaliação</p>
-          <p>5 Estelas</p>
-          <div className="flex">
-            {new Array(5).fill(0).map((_, i) => (
-              <StarIcon className="h-4 w-4 fill-white/80" key={i} />
-            ))}
-          </div>
-        </div>
-
-        <div className="flex w-full flex-col">
-          <p className="text-2xl font-bold">Já é cliente a:</p>
-          <p>Mais de 5 anos</p>
-        </div>
-      </div>
-    </div>
   );
 }
