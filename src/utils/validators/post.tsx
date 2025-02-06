deaa828d484fca574498e0534e38a92d2cdb123e
manu <victorx811@gmail.com>
2025-02-06 10:08:58 -0300

    improving UX

--- src/utils/validators/post.tsx
+++ src/utils/validators/post.tsx
@@ -4,7 +4,12 @@ export const postValidator = z.object({
   name: z.string().min(1, { message: "Você precisa informar o nome" }),
   description: z.string().optional(),
   image: z.string().optional(),
-  videoUrl: z.string().optional(),
+  videoUrl: z
+    .string()
+    .min(1, {
+      message:
+        "Você precisa informar a url do vídeo, coloque o vídeo como um SHORT do youtube, copie a URL dele e cole aqui.",
+    }),
   usginVideo: z.boolean(),
   rate: z.string().min(1, { message: "Você precisa informar a nota" }),
   membershipDuration: z.string().min(1, {
